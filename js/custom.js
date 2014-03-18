9069ece323e7632b2e2f43efc058494c8807260f
Denis Gursky <denis.gursky@itechart-group.com>
2014-03-18 12:02:41 +0300

    added devices

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,43 @@
 (function(){
 	window.addEventListener('load', function(){
+
+		var deviceMap = {
+			mobile: {
+				width: 320,
+				height: 480
+			},
+			tablet: {
+				height: 768,
+				width: 1024
+			},
+			desktop: {
+				width: 1432,
+				height: 800
+			}
+		};
+
+		var changeMode = function($device, $iframe, deviceType) {
+			$device.className = $device.className.replace(/mobile|tablet|desktop/, '') + ' ' +deviceType;
+
+			var device = deviceMap[deviceType];
+
+			$iframe.width =device.width;
+			$iframe.height =device.height;
+		};
+
+		var controlContainers = document.querySelectorAll('.control-container');
+
+		for(var i = 0; i < controlContainers.length; i++){
+			controlContainers[i].addEventListener('click', function(e){
+				var $device = e.currentTarget.getElementsByClassName('device')[0];
+
+				if(e.target.attributes['data-device']) {
+					var $iframe = $device.getElementsByTagName('iframe')[0],
+						deviceType = e.target.attributes['data-device'].value;
+
+					changeMode($device, $iframe, deviceType);
+				}
+			}, false);
+		}
 	}, false);
 })();
